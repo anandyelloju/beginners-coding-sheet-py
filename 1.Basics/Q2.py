ea5ee44877ed1524ec9e66a383a0660b54ccddb3
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-07-11 10:52:47 +0530

    Add Q2.py to Basics

---
+++ 1.Basics/Q2.py
@@ -0,0 +1,7 @@
+# 2) Write a Program to Print Integer Number Entered by User 
+
+user_input = input("Enter something:") 
+if user_input.isdigit():
+    print(user_input) 
+else: 
+    print("Not a number")
