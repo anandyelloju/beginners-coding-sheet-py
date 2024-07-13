db3c90ce87ce980b71d1b869e01752ee110ee324
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-07-13 09:29:21 +0530

    Add Q4,.py to Basics

---
+++ 1.Basics/Q4.py
@@ -0,0 +1,5 @@
+# 4) Write a program where the user is asked to enter two integers (divisor and dividend) and the quotient and the remainder of their division is computed.(Both divisor and dividend should be integers.)
+
+Num1 = int(input("Enter a integer(Dividend):  "))
+Num2 = int(input("Enter a integer(Divisor): "))
+print("Quotient:", Num1%Num2, ", Remainder", Num1/Num2)
