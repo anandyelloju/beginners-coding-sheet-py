a6561bc1f2553c3228bf7dc44788899016f80346
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-07-17 08:33:09 +0530

    Add Q8.py to Basics

---
+++ 1.Basics/Q8.py
@@ -0,0 +1,5 @@
+# 8) Write a Program to Multiply two decimal Numbers entered by User 
+
+Num1 = float(input("Enter a integer(Multiplicand):  "))
+Num2 = float(input("Enter a integer(Multiplier): "))
+print("Two decimal multiplication is ", Num1*Num2)
