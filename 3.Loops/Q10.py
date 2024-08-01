d624cce71edce2eca57fc840c933522a04821641
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-08-01 15:03:39 +0530

    Add Q10.py to Loops

---
+++ 3.Loops/Q10.py
@@ -0,0 +1,4 @@
+# 10) Write a Program to Calculate Power of a Number using inbuilt pow() function by taking two inputs from users as Base and exponent respectively
+
+base, expo = map(int, input("Enter base and expo: ").split())
+print(pow(base, expo))
